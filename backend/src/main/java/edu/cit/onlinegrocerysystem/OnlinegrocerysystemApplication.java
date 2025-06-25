fa0ef83d6adcf0745c75004498c9b19183ac706c
LAB12_USER ON WS20 <L12X15W20@citulab.net>
2025-06-25 16:19:31 +0800

    added backend

---
+++ backend/src/main/java/edu/cit/onlinegrocerysystem/OnlinegrocerysystemApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.onlinegrocerysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinegrocerysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinegrocerysystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/cit/onlinegrocerysystem/OnlinegrocerysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.onlinegrocerysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinegrocerysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
